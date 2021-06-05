331e617245b5d782e3d69af3d0baf6a4f9d625a7
nima <nimashahbazi524@gmail.com>
2021-06-05 16:33:39 +0430

    Init commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true
+  },
+  purge: [],
+  theme: {
+    extend: {
+      "plt-yellow": "#fab708",
+      "plt-white": "#eeeeee",
+      "plt-dark": "#333333",
+      "plt-darker": "#272727"
+    }
+  },
+  variants: {},
+  plugins: []
+};
