f18157e3135296a6d55a62ac3772bef701aed3df
nima <nimashahbazi524@gmail.com>
2021-07-22 21:39:06 +0430

    Add services api

--- pages/services/index.vue
+++ pages/services/index.vue
@@ -5,13 +5,21 @@
       sub_title="در اینجا میتوانید لیست کامل خدمات من را مشاهده کنید"
     />
     <div class="flex flex-wrap justify-around items-center my-16">
-      <service-post v-for="i in 9" :key="i" :index="i" />
+      <service-post v-for="(service, index) in services" :key="service" :index="index" />
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  async asyncData({ $axios }) {
+    const res = await $axios.get("http://api.ehsanmoqimi.ir/Service/services/")
+    console.log(res.data.data);
+    return {
+      services: res.data.data
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped></style>
